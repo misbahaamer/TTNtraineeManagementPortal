215943359c3465f793caeaecb4d5005b381f5fb0
Misbah Aamer <misbahuddin.aamer@gmail.com>
2019-07-05 21:14:31 -0400

    mongodb connected

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -1,5 +1,18 @@
-const express = require('express')
-const router  = express.Router()
+const express  = require('express')
+const router   = express.Router()
+const mongoose = require('mongoose')
+const db = "mongodb+srv://ttnuser:Sterling@123@cluster0-5owqi.mongodb.net/test?retryWrites=true&w=majority"
+
+
+mongoose.connect(db, { useNewUrlParser: true }, (err) => {
+    if (err) {
+        console.error('Error!' + err)
+    } else {
+        console.log('connected to mongodb')
+    }
+})
+
+
 
 router.get('/', (req, res) => {
     res.send('bismilllah,From API route')
